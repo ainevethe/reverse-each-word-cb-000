c017d675ffcccdaf790771cb6e760cdf74937a97
Crista Cooper <aine3d@gmail.com>
2017-11-18 01:36:41 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@
 def reverse_each_word(sentence)
   words = sentence.split
 
-  words.each do |word|
-    return (word.reverse!).to_s
+  words.each do |word.to_s|
+    return (word.reverse!)
   end
 end
